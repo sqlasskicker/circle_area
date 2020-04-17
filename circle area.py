bd2201ca7a28e2d5c951e9bf1a4d452918d2025d
sqlasskicker <63858650+sqlasskicker@users.noreply.github.com>
2020-04-17 19:38:36 +0530

    Add files via upload

---
+++ circle area.py
@@ -0,0 +1,11 @@
+# Enter Radius in input 
+# πr²
+
+π = 3.141592654
+print ("Enter Radii of the circle")
+r = int(input())
+print ("\nRadius of circle:\n")
+print (r)
+print ("\nArea of Circle will be:\n")
+print (r*π)
+print ("\nP3AC3✌️")
